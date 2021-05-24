9997108aa3f74c96c4f372a4e807b3e37a99d821
alumno <alumno@utparras.edu.mx>
2021-05-24 15:13:26 -0500

    Link para administrative

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import AdministrativeAdministrative from '../views/Administrative/Administrative.vue'
 
 Vue.use(VueRouter)
 
@@ -19,6 +20,11 @@ const routes = [
     component: function () {
       return import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
+  },
+  {
+    path:'/administrative/administrative',
+    name:'Administrativo',
+    component:AdministrativeAdministrative
   }
 ]
 
