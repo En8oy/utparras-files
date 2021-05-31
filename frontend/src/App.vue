ec1230c84f0913475febb4b8f1d67baf17bbee99
alumno <alumno@utparras.edu.mx>
2021-05-31 15:01:17 -0500

    app edit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <v-app>
+    <v-app-bar
+      app
+      dark
+      color="#2ea69a"
+      
+    >
+      <v-spacer></v-spacer>
+
+      <v-btn
+        to="/about"
+        text
+      >
+        <span class="mr-2 white--text">Iniciar Sesion</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+
+      
+    </v-app-bar>
+
     <v-main>
       <router-view/>
     </v-main>
@@ -13,104 +32,11 @@
       flat
       tile
     >
-    <v-card
-    
-    height="1000"
-  >
-    <v-app-bar
-      color="#009688"
-    >
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>
-        <v-img
-          contain
-          src="https://utparras.edu.mx/assets/images/utp-logo.png"
-          transition="scale-transition"
-          width="90"
-         
-        />
-        
-      </v-toolbar-title>
-      {{$route.name}}
-    </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-        <center>
-          <br>
-          <br>
-        <v-img
-          contain
-          src="https://utparras.edu.mx/assets/images/utp-logo.png"
-          transition="scale-transition"
-          width="90"
-         
-        />
-        <br>
-        
-        <h1>Expedientes UTParras</h1>
-        <br>
-        <br>
-        
-          <v-list-item>
-          
-            
-        
-            
-            <v-list-item-icon>
-              <v-icon size="40">mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Inicio</v-list-item-title>
-          </v-list-item>
-          
-          <br>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon size="40">mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Acerca de</v-list-item-title>
-          </v-list-item>
-          <br>
-
-
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon size="40">mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>iniciar sesion</v-list-item-title>
-          </v-list-item>
-          </center>
-          <br>
-          
-    
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-    <div>
-      <router-view></router-view>
-    </div>
-  </v-card>
-    
-    
       <v-card-title class="teal">
-        
         <h3 class="subheading">Universidad Tecnologica De Parras de la Fuente</h3>
-        
+
         <v-spacer></v-spacer>
-        
+
         <v-btn
           v-for="icon in icons"
           :key="icon.name"
@@ -119,16 +45,14 @@
           icon
           :href="icon.url"
           target="_blank"
-    
         >
-        
-          <v-icon size="35px">
+          <v-icon size="24px">
             {{ icon.icon }}
-            
           </v-icon>
         </v-btn>
-        
       </v-card-title>
+      
+
     
     </v-card>
   </v-footer>
@@ -136,7 +60,6 @@
   </v-app>
   
 </template>
-
 
 
 
