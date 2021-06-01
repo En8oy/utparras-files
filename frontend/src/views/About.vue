8d972e5fb42414df1350ff4d3c4b9485c385aa62
jorgetovar2 <jorgetovar765@gmail.com>
2021-05-31 23:27:28 -0500

    corrección de header y footer

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,70 +1,5 @@
   <template>
- <center><v-row>
-
-    <v-col
-      cols="12"
-      sm="6"
-    >
-    <v-btn
-  color="secondary"
-  elevation="11"
-  x-large
-></v-btn>
  
-
-
-
-
-
-    </v-col>
-  </v-row> 
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <v-footer
-    dark
-    padless
-     >
-        <v-card
-        class="flex"
-        flat
-        tile
-        cols="12"
-           sm="12"
-           md="12"
-        >
-            <v-card-title class="teal">
-                <strong class="subheading"><h2>Universidad Tecnológica de Parras de la Funte</h2></strong>
-
-                <v-spacer></v-spacer>
-
-            <v-btn
-            v-for="icon in icons"
-            :key="icon.name"
-            class="mx-4"
-            dark
-            icon
-            :href="icon.url"
-            target="_blank"
-            cols="12"
-           sm="12"
-           md="12"
-        >
-          <v-icon size="35px">
-            {{ icon.icon }}
-           
-          </v-icon>
-        </v-btn>
-            </v-card-title>
-
-            <v-card-text class="py-2 white--text text-center">
-                {{ new Date().getFullYear() }} — <strong>UTP</strong>
-            </v-card-text>
-        </v-card>
-    </v-footer></center>
   
   
 </template>
@@ -73,9 +8,4 @@
 
 
 
-<style lang="sass" scoped>
-.v-card.on-hover.theme--dark
-  background-color: rgba(#FFF, 0.8)
-  >.v-card__text
-    color: #000
-</style>
+
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,51 +1,42 @@
 <template>
-  <div>
-    <v-row>
-      <v-col sm="12">
-        <v-img
-          src="https://www.utparras.edu.mx/assets/images/edificio.JPG"
-          height="830px"
-        >
-          <template border="12">
-            <v-row class="fill-height mt-16" align="center" justify="center">
-              <v-col sm="12" class="mt-12">
-               
-                
-                <h1>BIENVENIDO</h1>
-                 
-                
-              </v-col>
-            </v-row>
-          </template>
-        </v-img>
-      </v-col>
-    </v-row>
-  </div>
-  <!-- <div>
+  <center><div>
       <v-container fluid>
         <v-layout wrap>
           <v-flex>
-            <v-img
-              src="https://www.utparras.edu.mx/assets/images/edificio.JPG"
-              height="830px"
-            >
+            
               <v-row>
                 <v-col sm="12">
-                  <v-card class="pa-16">
-                    <v-card-title>
-                      BIENVENIDO A EXPEDIENTES UTPARRAS
-                    </v-card-title>
-                  </v-card>
+                  <v-img 
+                          src="https://www.utparras.edu.mx/assets/images/edificio.JPG">
+
+                          <br />
+                          <br />
+                          <br />
+
+                        
+                          <h1
+                          class="white--text"
+                          >BIENVENIDO A EXPEDIENTES UTPARRAS</h1>
+                          
+                          <br />
+                          <br />
+                          <br />
+                          <v-btn
+                          color="red"
+                          elevation="10"
+                          width="250"
+                          dark
+                          to="/about"
+                          >INICIAR</v-btn>
+                  </v-img>
+
                 </v-col>
               </v-row>
-            </v-img>
+            
           </v-flex>
         </v-layout>
       </v-container>
-    </div> -->
-  <!-- <center class="white--text text-center">
-      <h1>BIENVENIDO A EXPEDIENTES UTPARRAS</h1>
-    </center> -->
+    </div></center>
 </template>
 
 
