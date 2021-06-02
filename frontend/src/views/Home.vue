d145875850bd035b739f5c058d5d051c950dfecc
alumno <alumno@utparras.edu.mx>
2021-06-02 14:39:20 -0500

    estadisticas.home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,52 @@
 <template>
   <div>
-    <h1>Hola Soy Home</h1>
+    <h1 class="text-center">Estadisticas de Expedientes UTParras</h1>
+    <div>
+      <div cols="6" class="row">
+        <div xs="12" class="col-md-6 col-12">
+          <div class="v-card v-sheet theme--light elevation-10">
+            <div tabindex="-1" class="v-list-item v-list-item--three-line theme--light">
+              <div class="v-list-item__content">
+                <div class="overline mb-4 pa-5 white--text teal"> <h1 class="text-center">Personal Docente</h1> </div>
+                <p class="ma-3 text-center"> 35 </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        <div xs="12" class="col-md-6 col-12">
+          <div class="v-card v-sheet theme--light elevation-10">
+            <div tabindex="-1" class="v-list-item v-list-item--three-line theme--light">
+              <div class="v-list-item__content">
+                <div class="overline mb-4 pa-5 white--text teal"> <h1 class="text-center">Departamentos</h1> </div>
+                <p class="ma-3 text-center"> 6 </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div cols="6" class="row">
+        <div xs="12" class="col-md-6 col-12">
+          <div class="v-card v-sheet theme--light elevation-10">
+            <div tabindex="-1" class="v-list-item v-list-item--three-line theme--light">
+              <div class="v-list-item__content">
+                <div class="overline mb-4 pa-5 white--text teal"> <h1 class="text-center">Edificios</h1> </div>
+                <p class="ma-3 text-center"> 4 </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        <div xs="12" class="col-md-6 col-12">
+          <div class="v-card v-sheet theme--light elevation-10">
+            <div tabindex="-1" class="v-list-item v-list-item--three-line theme--light">
+              <div class="v-list-item__content">
+                <div class="overline mb-4 pa-5 white--text teal"> <h1 class="text-center">Maestros</h1> </div>
+                <p class="ma-3 text-center"> 18 </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
