2d482d4af488b6c4111d4686d291196cf0766480
alumno <alumno@utparras.edu.mx>
2021-06-03 14:50:27 -0500

    edit home , historia
    historia se modifico la informacion y se agrego un  carrusel con imagenes de la institucion.
    
    home se agrego pasrte de las estadisticas.

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,50 +1,116 @@
 <template>
   <center>
-    <v-row justify="center" >
+    <v-row justify="center">
       <v-col class="float-left mr-16" cols="9" sm="10" md="9">
         <v-card-text class="my-4 text-center title">
           <br />
 
-          
-          
-            <h1>HISTORIA</h1>
-            <br />
+          <!-- <center>
+            <v-img
+              class="mr-16"
+              cols="9"
+              sm="10"
+              md="9"
+              width="100%"
+              src="https://scontent.fslw1-1.fna.fbcdn.net/v/t1.6435-9/34019879_1797837106921654_4399235100353495040_n.png?_nc_cat=105&ccb=1-3&_nc_sid=973b4a&_nc_ohc=FWZ_P2qGx1oAX8WFiyk&_nc_ht=scontent.fslw1-1.fna&oh=d6d3562794159b8fcf3c3a1a6819b952&oe=60E047B9"
+            >
+              <br />
+              <br />
+              <br />
+            </v-img>
+          </center> -->
 
-            <p class="text-justify">
-              Los antecedentes de la Universidad Tecnológica de Parras de la
-              Fuente se remontan a 1999, en la Unidad Academica de la
-              Universidad Tecnológica de Coahuila, cuando se atendió una demanda
-              de la región ante la falta de oportunidades para los egresados del
-              nivel medio superior. Aunque se debió atender hasta el 26 de Enero
-              del 2016 para que se decretara su constitución formal en el Diario
-              Oficial y que, al abrir sus puertas, atendiera a 452 alumnos, con
-              una oferta de cinco programas educativos, uno de ellos, cabe
-              destacar, el primero a nivel nacional.
-            </p>
-            <br />
+          <v-carousel  :show-arrows="false" hide-delimiters cycle>
+            <v-carousel-item
+            
+              v-for="(item, i) in items"
+              :key="i"
+              :src="item.src"
+              reverse-transition="fade-transition"
+              transition="fade-transition"
+            ></v-carousel-item>
+          </v-carousel>  
 
-            <h1>ACERCA DEL SITIO</h1>
-            <br />
-            <br />
+          <br />
+          <br />
 
-            <p class="text-justify">
-              Este sitio cuenta con la utilidad de registrar datos del personal
-              de la Universidad Tecnológica de Parras de la Fuente, con el fin
-              de tener un mejor control a la hora de obtener información, el
-              sitio ayuda directamente a obtener esos datos sin la necesidad de
-              acudir personalmente.
-            </p>
-            <v-img 
-     width="800"
-     height="400"
-     
-    src="https://pbs.twimg.com/media/DHXBzgFUMAAPa5R.jpg"
-    >
-     </v-img>
-          
+          <h1>Antecedentes</h1>
+          <br />
+
+          <p class="text-justify">
+            Los antecedentes de la Universidad Tecnológica de Parras de la
+            Fuente se remontan a 1999, en la Unidad Academica de la Universidad
+            Tecnológica de Coahuila, cuando se atendió una demanda de la región
+            ante la falta de oportunidades para los egresados del nivel medio
+            superior. Aunque se debió atender hasta el 26 de Enero del 2016 para
+            que se decretara su constitución formal en el Diario Oficial y que,
+            al abrir sus puertas, atendiera a 452 alumnos, con una oferta de
+            cinco programas educativos, uno de ellos, cabe destacar, el primero
+            a nivel nacional.
+          </p>
+          <br />
+          <br />
+          <br />
+
+          <h1>Impacto Social</h1>
+          <br />
+
+          <p class="text-justify">
+            La Universidad cubre la necesidad de la industria manufacturera, la
+            cual require perfiles de nivel técnico superior para atender las
+            necesidades de operación de fábrica como Dickies de Parras, Cone y
+            Star del Norte, entre otras, como las vitivinicolas Casa Madero y
+            Casa Rivero, así como las necesidades que tienen los comercios y
+            negocios de la región.
+          </p>
+          <br />
+          <br />
+          <br />
+
+          <h1>ACERCA DEL SITIO</h1>
+          <br />
+          <br />
+
+          <p class="text-justify">
+            Este sitio cuenta con la utilidad de registrar datos del personal de
+            la Universidad Tecnológica de Parras de la Fuente, con el fin de
+            tener un mejor control a la hora de obtener información, el sitio
+            ayuda directamente a obtener esos datos sin la necesidad de acudir
+            personalmente.
+          </p>
         </v-card-text>
       </v-col>
     </v-row>
   </center>
 </template>
 
+<script>
+export default { 
+  data() {
+    return {
+      items: [
+        {
+          src: 
+            "https://scontent.fslw1-1.fna.fbcdn.net/v/t1.6435-9/122306967_3498534646851883_3363711158602919916_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=973b4a&_nc_ohc=nhsFx3xqEioAX8G_T4G&_nc_ht=scontent.fslw1-1.fna&oh=c095e8e50e217c55a9a21f1794abe560&oe=60DE1963",
+        },
+        {
+          src:
+            "https://scontent.fslw1-1.fna.fbcdn.net/v/t1.6435-9/84485162_2828525067186181_2037803450414137344_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=973b4a&_nc_ohc=I4tBcbzskPoAX9nNLiP&_nc_ht=scontent.fslw1-1.fna&oh=9c6a0d67473fcab61f1bec0843cb6e0c&oe=60E04B8B",
+        },
+        {
+          src:
+            "https://scontent.fslw1-1.fna.fbcdn.net/v/t1.6435-9/64359515_2358613607510665_8087101270050996224_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=973b4a&_nc_ohc=1zdjnBzB6VUAX8z0a-I&_nc_ht=scontent.fslw1-1.fna&oh=b2698d1449910e28ce3e40333ce8bc1c&oe=60DCCB2F",
+        },
+        {
+          src:
+            "https://scontent.fslw1-1.fna.fbcdn.net/v/t1.6435-9/34019879_1797837106921654_4399235100353495040_n.png?_nc_cat=105&ccb=1-3&_nc_sid=973b4a&_nc_ohc=FWZ_P2qGx1oAX8WFiyk&_nc_ht=scontent.fslw1-1.fna&oh=d6d3562794159b8fcf3c3a1a6819b952&oe=60E047B9",
+        },
+        {
+          src:
+            "https://scontent.fslw1-1.fna.fbcdn.net/v/t1.6435-9/91050179_2937628412942512_6329177989091164160_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=973b4a&_nc_ohc=95kqk6JsJH0AX8lXC4t&_nc_ht=scontent.fslw1-1.fna&oh=6cb0f25116fb1f3405f6d606608a2aee&oe=60DD5749",
+        },
+      ],
+    };
+  },
+};
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,25 +1,164 @@
 <template>
- <v-flex>
+  <v-flex>
     <v-row>
-    <v-col cols="9" sm="10" md="12">
-    <br>
-    <br>
-    
+      <v-col cols="9" sm="10" md="12">
+        <br />
+        <br />
 
-    <center>
-    <h1>BIENVENIDO A EXPEDIENTES UTPARRAS</h1></center>
-    <br>
-    <br>
-    
-     <center><v-img 
-     width="800"
-     height="400"
-     
-    src="https://expertosis.com/wp-content/uploads/2020/03/aodocs-documentos.gif"
-    >
-     </v-img></center>
+        <center>
+          <h1>BIENVENIDO A EXPEDIENTES UTPARRAS</h1>
+        </center>
+        <br />
+        <br />
 
-    </v-col>
+        <center>
+          <v-img
+            width="800"
+            height="400"
+            src="https://expertosis.com/wp-content/uploads/2020/03/aodocs-documentos.gif"
+          >
+          </v-img>
+        </center>
+        <br>
+        <br>
+        <br>
+        
+
+        <div cols="9" sm="12" md="12">
+          <center>
+            <div>
+              <div cols="6" class="row">
+                <div xs="12" class="col-md-6 col-12">
+                  <div class="v-card v-sheet theme--light elevation-10">
+                    <div
+                      tabindex="-1"
+                      class="v-list-item v-list-item--three-line theme--light"
+                    >
+                      <div class="v-list-item__content">
+                        <div class="overline mb-4 pa-5 white--text teal">
+                          <h1 class="text-center">Personal Docente</h1>
+                        </div>
+                        <h1 class="ma-3 text-center">35</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div xs="12" class="col-md-6 col-12">
+                  <div class="v-card v-sheet theme--light elevation-10">
+                    <div
+                      tabindex="-1"
+                      class="v-list-item v-list-item--three-line theme--light"
+                    >
+                      <div class="v-list-item__content">
+                        <div class="overline mb-4 pa-5 white--text teal">
+                          <h1 class="text-center">Departamentos</h1>
+                        </div>
+                        <h1 class="ma-3 text-center">6</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div cols="6" class="row">
+              <div xs="12" class="col-md-6 col-12">
+                <div class="v-card v-sheet theme--light elevation-10">
+                  <div
+                    tabindex="-1"
+                    class="v-list-item v-list-item--three-line theme--light"
+                  >
+                    <div class="v-list-item__content">
+                      <div class="overline mb-4 pa-5 white--text teal">
+                        <h1 class="text-center">Edificios</h1>
+                      </div>
+                      <h1 class="ma-3 text-center">4</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div xs="12" class="col-md-6 col-12">
+                <div class="v-card v-sheet theme--light elevation-10">
+                  <div
+                    tabindex="-1"
+                    class="v-list-item v-list-item--three-line theme--light"
+                  >
+                    <div class="v-list-item__content">
+                      <div class="overline mb-4 pa-5 white--text teal">
+                        <h1 class="text-center">Maestros</h1>
+                      </div>
+                      <h1 class="ma-3 text-center">18</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </center>
+          <br /><br /><br />
+          <center>
+            <div cols="12" sm="12" md="12">
+              <div id="responsive-chart"></div>
+              <apexchart
+                width="900"
+                type="bar"
+                :options="options"
+                :series="series"
+              >
+              </apexchart>
+            </div>
+          </center>
+        </div>
+      </v-col>
     </v-row>
-    </v-flex>
+  </v-flex>
 </template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: () => ({
+    options: {
+      chart: {
+        id: "vuechart-example",
+        height: 400,
+        type: "bar",
+        align: "left",
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+        },
+      },
+
+      xaxis: {
+        categories: [
+          "Personal Docente",
+          "Departamentos",
+          "Edificios",
+          "Maestros",
+        ],
+      },
+      title: {
+        text: "Estadistica UTParras",
+        align: "center",
+        style: {
+          fontSize: "20px",
+        },
+      },
+      colors: ["#00897b"],
+    },
+    responsive: [
+      {
+        breakpoint: undefined,
+        options: {},
+      },
+    ],
+    series: [
+      {
+        name: "En Existencia",
+        data: [35, 6, 4, 18],
+      },
+    ],
+  }),
+};
+</script>
