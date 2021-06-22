a0735a9933ceee4c1336d53839e1e68158c424b8
Irbing Alejandro Moreno Garcia <31268601+En8oy@users.noreply.github.com>
2021-06-21 20:18:09 -0500

    Some changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,7 @@
-
 import Vue from "vue";
 import VueRouter from "vue-router";
 import NotFound from "../components/NotFound.vue"
 import store from "@/store"
-import jspdf from 'jspdf'
 
 // Public Routes
 import Home from "../views/Home.vue";
@@ -25,79 +23,78 @@ import AdministrativeHome from "../views/Administrative/Home.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "*",
-    name: "Pagina No Encontrada",
-    component: () => import(
-      /*webpackChunkName: "ExperienceDetails"*/
-      "../components/NotFound")
-  },
-  // Public Routes
-  {
-    path: "/",
-    name: "Bienvenido a Expedientes UTParras",
-    component: Home,
-    
-  },
-  {
-    path: "/nosotros",
-    name: "Acerca De Nosotros",
-    component: About,
-  },
-  {
-    path: "/login",
-    name: "Inicia Sesión",
-    component: Login,
-  },
-  {
-    path: "/expedientes",
-    name: "Expedientes Publicos",
-    component: File,
-  },
-  // Administrator Routes
-  {
-    path: "/administrador/inicio",
-    name: "Bienvenido Administrador",
-    component: AdministratorHome,
-    
-  },
-  {
-    path: "/administrador/usuarios",
-    name: "Administrar Usuarios",
-    component: AdministratorUser,
-  },
-  // Administrative Routes
-  {
-    path: "/administrativo/inicio",
-    name: "Bienvenido Administrador",
-    component: AdministrativeHome,
-  },
-  // Teacher Routes
-  {
-    path: "/maestro/inicio",
-    name: "Bienvenido Administrador",
-    component: TeacherHome,
-  },
+const routes = [{
+        path: "*",
+        name: "Pagina No Encontrada",
+        component: () =>
+            import (
+                /*webpackChunkName: "ExperienceDetails"*/
+                "../components/NotFound")
+    },
+    // Public Routes
+    {
+        path: "/",
+        name: "Bienvenido a Expedientes UTParras",
+        component: Home,
+
+    },
+    {
+        path: "/nosotros",
+        name: "Acerca De Nosotros",
+        component: About,
+    },
+    {
+        path: "/login",
+        name: "Inicia Sesión",
+        component: Login,
+    },
+    {
+        path: "/expedientes",
+        name: "Expedientes Publicos",
+        component: File,
+    },
+    // Administrator Routes
+    {
+        path: "/administrador/inicio",
+        name: "Bienvenido Administrador",
+        component: AdministratorHome,
+
+    },
+    {
+        path: "/administrador/usuarios",
+        name: "Administrar Usuarios",
+        component: AdministratorUser,
+    },
+    // Administrative Routes
+    {
+        path: "/administrativo/inicio",
+        name: "Bienvenido Administrador",
+        component: AdministrativeHome,
+    },
+    // Teacher Routes
+    {
+        path: "/maestro/inicio",
+        name: "Bienvenido Administrador",
+        component: TeacherHome,
+    },
 ];
 
 beforeEnter: (to, from, next) => {
-  const exists = store.destinations.find(
-    destination => destination.slug === to.params.slug
-  );
-  if (exists) {
-    next();
-  } else {
-    next({ name: "notFound" });
-  }
+    const exists = store.destinations.find(
+        destination => destination.slug === to.params.slug
+    );
+    if (exists) {
+        next();
+    } else {
+        next({ name: "notFound" });
+    }
 }
 
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
 });
 
 export default router;
-
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,18 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import jspdf from 'jspdf'
 
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    url: "http://127.0.0.1:8000/api/"
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        url: "http://127.0.0.1:8000/api/"
+    },
+    mutations: {},
+    actions: {},
+    modules: {}
 })
