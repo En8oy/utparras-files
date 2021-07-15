63ac7467a69fdf06708f0e59a165f5084bcfd8d0
jorgetovar2 <jorgetovar765@gmail.com>
2021-07-15 14:33:43 -0500

    Revert "Revert "Merge branch 'Jorge' of https://github.com/En8oy/utparras-files into Jorge""
    This reverts commit 5432ae696eac4af76651411eec380f018b90172b.

---
+++ frontend/src/store/modules/auth.js
@@ -0,0 +1,60 @@
+export default {
+    namespaced: true,
+    state: {
+        token: "",
+        user: {
+            surname: "",
+            name: "",
+            birthdate: "",
+            personal_email: "",
+            curp: "",
+            rfc: "",
+            civil_status: "",
+            sexo: "",
+            personal_address: "",
+            origin: "",
+            full_name: "",
+            family: "",
+            phone: "",
+            address_family: "",
+            instituciones: "",
+            career: "",
+            entry: "",
+            egress: "",
+            experiences: "",
+            position: "",
+            type_exp: "",
+            start: "",
+            end: "",
+            skill: "",
+            level: "",
+            institucion: "",
+            responsable: "",
+            email_res: "",
+            tel: "",
+            resp: "",
+            horario: "",
+        },
+    },
+    mutations: {
+        SET_LOGIN({ state }, payload) {
+            this.token = payload.token
+            this.user = payload.user
+        }
+    },
+    actions: {
+        login({ state, rootState, commit }, payload) {
+            return new Promise((resolve, reject) => {
+                // console.log(payload, "Vuex")
+                // axios.post(state.baseUrl + "login", payload)
+                //     .then(res => {
+                //         commit("SET_LOGIN", res.data)
+                //         resolve(res.data)
+                //     })
+                //     .catch(error => {
+                //         reject(error)
+                //     })
+            })
+        },
+    }
+}
